8423c051e7a2cae36c29954b380492b489ac3f90
Nitiksh Kumar <122013711+nitikshh@users.noreply.github.com>
2024-08-04 18:26:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -226,4 +226,5 @@ def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
