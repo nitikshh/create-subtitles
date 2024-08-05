4b913e042e5b4fef0b750cd0b55b2b4c2a199072
Nitiksh Kumar <122013711+nitikshh@users.noreply.github.com>
2024-08-05 06:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -233,4 +233,5 @@ def download_file(filename):
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
